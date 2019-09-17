8202bc4e96a250a6d48dfe874eb9c9ec04758e45
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 14:40:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,9 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    students = {}
+    students = []
     doc.css("student-card").each do |card|
-      students[card.css("h4.student-name").text] = {}
+      students << {name:card.css("h4.student-name").text}
     end
     students
   end
