44cd3ace9b6a44d9874d6299db2be5c248f94941
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 14:38:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = {}
+    doc.css("student-card").each do |card|
+      students[card.css("h4.student-name").text] = {}
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
